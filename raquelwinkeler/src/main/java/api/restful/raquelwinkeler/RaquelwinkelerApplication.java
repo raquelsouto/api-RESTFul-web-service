0ebe20ee32c6ba11a7c55ef04004e66e913a7f01
Raquel Souto <kelsouto@gmail.com>
2023-01-09 01:15:10 -0300

    Adicionar projeto

---
+++ raquelwinkeler/src/main/java/api/restful/raquelwinkeler/RaquelwinkelerApplication.java
@@ -0,0 +1,13 @@
+package api.restful.raquelwinkeler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaquelwinkelerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaquelwinkelerApplication.class, args);
+    }
+
+}
---
+++ raquelwinkeler/src/test/java/api/restful/raquelwinkeler/RaquelwinkelerApplicationTests.java
@@ -0,0 +1,13 @@
+package api.restful.raquelwinkeler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaquelwinkelerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
